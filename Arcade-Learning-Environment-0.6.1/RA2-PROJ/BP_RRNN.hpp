05bd7909f119dc276e719acc048a9db482329b80
victoria <victoria>
2026-01-16 21:16:25 +0100

    Creada carpeta FINAL

---
+++ Arcade-Learning-Environment-0.6.1/RA2-PROJ/BP_RRNN.hpp
@@ -0,0 +1,300 @@
+// esto define lo acordado en llamada que es la red neuronal
+#ifndef __BP_RRNN_HPP__
+#define __BP_RRNN_HPP__
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+class Neurona
+{
+
+public:
+    // como es una red que deberia ser dinamica voy a poner vector de pesos que puedan cambiar
+    // para mantener el orden, si la neurona no es apuntada por la neurona correspondiente en orden, le ponemos 0
+    // tamaño del vector de pesos sera el tamaño (numero de neuronas) de la capaa anterior
+    vector<double> pesos;
+
+    double bias;
+
+    double salida;   // salida de la neurona
+    double delta_bp; // delta del backpropagation que es el error
+
+    // la salida de la neurona no seria necesario ponerla creo porque seria la entrada de la siguiente capa, o sea que si es la ultima capa, esa ya tiene una entrada y ninguna salida.
+
+    Neurona(size_t tam_capa_anterior) : pesos(tam_capa_anterior, 0.0), bias(0.0), salida(0.0), delta_bp(0.0)
+    {
+
+        // partimos de que los pesos de una red se inicializan de forma aleatoria
+
+        bias = (((double)rand()) / RAND_MAX) - 0.5;
+
+        for (double &peso : pesos)
+        {
+            peso = (((double)rand()) / RAND_MAX) - 0.5;
+        }
+    }
+};
+
+class Capa
+{
+
+public:
+    vector<Neurona> neuronas;
+    // CONSTRUCTOR BASICO
+    Capa(size_t num_neuronas, size_t tam_capa_anterior)
+    {
+
+        for (size_t i = 0; i < num_neuronas; ++i)
+        {
+
+            neuronas.emplace_back(tam_capa_anterior);
+        }
+    }
+};
+
+class RedBackPropagation
+{
+public:
+    vector<Capa> capas;
+
+    double tasa_aprendizaje = 0.1; // es lo que dimos en DP, lo vamos a ir ajustando
+
+    // FUNC SIGMOIDEA Y LA DERIVADA
+
+    double sigmoid(double n)
+    {
+        double resultado = 1.0 / (1.0 + (exp(n * -1)));
+
+        return resultado;
+    }
+
+    double sigmoid_derivada(double n)
+    {
+        double resultado = n * (1.0 - n);
+
+        return resultado;
+    }
+
+    // es cantidad de capas
+    // o sea que si tenemos {2, 4, 3}
+    // tenemos 2 capas de entrada
+    // 4 capas ocultas
+
+    // 3 salidas (acciones: moverse a izquierda/ moverse a la derecha / atacar DEL PERSONAJE
+
+    RedBackPropagation(const vector<size_t> &arquitectura)
+    {
+
+        for (size_t i = 1; i < arquitectura.size(); ++i)
+        {
+            // el tamaño de la capa de entrada de la neurona de capa n tiene tamaño de la capa anterior
+            capas.emplace_back(arquitectura[i], arquitectura[i - 1]);
+        }
+    }
+
+    // hacia delante
+
+    vector<double> forward(const vector<double> &entrada)
+    {
+        //cout << endl;
+        //cout << "en forward " << endl;
+
+        int num_capa = 0;
+
+        // esto son los 11 parametros de las personas
+        vector<double> activaciones = entrada;
+
+        for (auto &capa : capas)
+        {
+
+            // VALORES QUE SALEN DE CADA CAPA
+            vector<double> nuevas_activaciones;
+
+            //cout << endl << "CAPA " << num_capa << endl;
+            int num_neurona = 0;
+
+            // por cada neurona de la capa en la que estamos,
+            for (auto &neurona : capa.neuronas)
+            {
+                // suma ponderada de las entradas y el bias
+
+                // por cada neurona, SUMA es su bias y la multiplicacion de los valores de la entrada de la red * los pesos de su vector de entrada (o sea que la señala desde otras neuronas)
+                double suma = neurona.bias;
+                //cout << "   num neurona " << num_neurona << endl;
+
+                // NUESTRO CONJUNTO DE DATS
+                for (size_t i = 0; i < activaciones.size(); i++)
+                {
+                    // cout << "añadiendo " << neurona.pesos[i] << " * " << activaciones[i] << endl;
+
+                    // activaciones tiene el tamaño de la capa anterior
+                    suma += neurona.pesos[i] * activaciones[i];
+                }
+
+                // neurona.salida = sigmoid(suma);
+                // cout << "       suma de la neurona " << suma << endl;
+
+                // SI ES LA CAPA DE SALIDA, me quedo son la suma como salida
+                // SI NO, SE VA A SIGMOID
+                bool es_salida = (&capa == &capas.back());
+                neurona.salida = es_salida ? suma : sigmoid(suma);
+
+                nuevas_activaciones.push_back(neurona.salida);
+
+                // si es una capa mas, pasa a ser el vector de entrada actualizao
+
+                // no hay problema porque el vector es tamaño igual al tamaño de la capa
+                // salida de la capa es de tamaño de la capa
+
+                num_neurona++;
+            }
+
+            //cout << "vector de entrada MODIFICADO por neuronas " << endl;
+            //cout << "[";// (deberia ser mismo largo que numero de neuronas de la capa)
+            for (int i = 0; i < nuevas_activaciones.size(); i++)
+            {
+                //cout << nuevas_activaciones[i] << ",";
+            }
+            //cout << "]" << endl;
+
+            activaciones = nuevas_activaciones;
+
+            num_capa++;
+
+
+
+
+
+            bool es_salida = (&capa == &capas.back());
+            if (es_salida){
+
+                //cout << "ESTO ES LA SALIDA ";
+                for (int i = 0; i < nuevas_activaciones.size(); i++){
+                    //cout << nuevas_activaciones[i] << ",";
+                }
+                //cout << endl << "SALIDA - OBJETIVO ES EL ERROR, objetivo es SUEÑO real" <<endl;
+            }
+        }
+
+
+        //cout << endl;
+
+        return activaciones;
+    }
+
+
+
+
+    
+
+    void backpropagation(const vector<double> &entrada, const vector<double> objetivo)
+    {
+        //cout << endl << "BACKPROPAGATION" << endl;
+        Capa &capa_salida = capas.back(); // salida.neuronas.size() ES EL TAMAÑO DE LA CAPA
+
+        for (size_t i = 0; i < capa_salida.neuronas.size(); i++)
+        {
+
+            double c_salida_output = capa_salida.neuronas[i].salida;
+
+            capa_salida.neuronas[i].delta_bp = (c_salida_output - objetivo[i]); //* sigmoid_derivada(c_salida_output);
+        }
+
+        // VOY HACIA ATRAS EN LAS CAPAS:
+
+        for (int j = capas.size() - 2; j >= 0; j--)
+        {
+
+            for (size_t i = 0; i < capas[j].neuronas.size(); i++)
+            {
+                //cout << "encontrar error en NEURONA " << i << " de la capa OCULTA " << j << endl;
+
+                double suma = 0.0;
+
+                // es la capa siguiente, la mas cercna a la salida
+                for (auto &neurona_siguiente : capas[j + 1].neuronas)
+                {
+
+                    suma += neurona_siguiente.pesos[i] * neurona_siguiente.delta_bp;
+                }
+
+                double capasnuevas_salidas = capas[j].neuronas[i].salida;
+
+                capas[j].neuronas[i].delta_bp = suma * sigmoid_derivada(capasnuevas_salidas);
+            }
+        }
+
+        // ACTUALIZO PESOS
+
+        vector<double> activaciones_previas = entrada;
+
+        for (auto &capa : capas)
+        {
+
+            for (auto &neurona : capa.neuronas)
+            {
+
+                for (size_t i = 0; i < neurona.pesos.size(); i++)
+                {
+
+                    // GRADIENTE DESCENDENTE
+                    neurona.pesos[i] -= neurona.delta_bp * tasa_aprendizaje * activaciones_previas[i];
+                }
+
+                // ESTIY EN NEURONA AHORA
+
+                neurona.bias -= tasa_aprendizaje * neurona.delta_bp;
+            }
+
+            // limpio el vector de activaciones previas
+
+            activaciones_previas.clear();
+            for (auto &n : capa.neuronas)
+            {
+
+                activaciones_previas.push_back(n.salida);
+            }
+        }
+    }
+
+    // EL ENTRENAMIENTO SE HACE CON TODO Y LA ULTIMA CAPA ES LA QUE DICE LO QUE SE HACE, PERO HAY QUE DARLE TODO PARA ENTRENARLA (activaciones)
+    // EL PREDICTOR DIRA EN BASE AL MOVIMIENTO DEL ENEMIGO, LO QUE EL JUGADOR DEBE HACER
+    // como la estructura sera asi
+    /*
+
+        {
+            [3, 4, 5, ...., 5],
+            [3, 4, 5, ...., 5],
+            [3, 4, 5, ...., 5]
+
+        }
+
+    y aun no sabemos los datos que vamos a incluir si o si, voy a hacer los dos vectores dinamicos
+
+    */
+
+    // EL CONJUNTO DE APRENDIZAJE TIENE INPUT Y OUTPUT: input es el movimiento del enemigo y output el movimiento del jugador PERO AMBOS SON EL 'INPUT' PARA ENTRENAR LA RED
+    // para el preductor, la idea es que el input sea datos del enemigo y output sea datos del personaje
+
+    // se suponr que 1 capa oculta es suficiente para los problemas de regresión
+    // en el print se ve 0 y 1 (capa de salida y oculta, la de entrada no es explicita)
+    void entrenar(const vector<vector<double>> input, const vector<vector<double>> output, int epocas)
+    {
+
+        for (int i = 0; i < epocas; i++)
+        {
+            cout << "Entrenamiento en ÉPOCA: " << i << endl;
+            // va a haber la misma cantidad de samples de datos enemigo y de jugador porque por cada ACCION DE ENEMIGO HAY UNA REACCION DE JUGADOR
+            for (size_t i = 0; i < input.size(); i++)
+            {
+
+                forward(input[i]);
+                backpropagation(input[i], output[i]);
+            }
+        }
+    }
+};
+
+#endif
---
+++ Arcade-Learning-Environment-0.6.1/RA2-PROJ/iris_bp.cpp
@@ -0,0 +1,396 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream> // Necesario para stringstream
+#include <string>
+#include <cstring>
+#include "BP_RRNN.hpp"
+#include <algorithm>  // para desordenar el vector y que pueda entrenarse con todas las categorias al separar datos
+#include <random> 
+using namespace std;
+
+/*
+
+    Iris-setosa
+    Iris-versicolor
+    Iris-virginica
+
+*/
+
+double arreglodato(string palabra, int tipo)
+{
+    double val = 0;
+
+    switch (tipo)
+    {
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+
+        //cout << "flotantes " << endl;
+        val = 0;
+        val = stof(palabra) ;
+        val = val / 100;
+        break;
+    case 4: { 
+        //cout << "clasificacion " << endl;
+        val = 0;
+        if (palabra == "Iris-setosa"){
+           val = 0; 
+        }
+        else if(palabra == "Iris-versicolor"){
+            val = 1;
+        }
+        else if(palabra == "Iris-virginica"){
+            val = 2;
+        }
+        else{
+
+            val = 0;
+        }
+
+       
+        
+        break;
+    }
+    
+    }
+
+    return val;
+    
+}
+
+
+int mayor_valor(double primero, double segundo, double tercero){
+
+    if(primero >= segundo && primero >= tercero){
+        return 0;
+    }
+    else if(segundo >= tercero){
+
+        return 1;
+    }
+    else{
+
+        return 2;
+    }
+}
+
+
+struct MConfusion{
+
+    // x_mal_y ; clase x que fue predecida como y
+    int c0 = 0;
+    int c0_mal_c1= 0;
+    int c0_mal_c2= 0;
+
+    int c1= 0;
+    int c1_mal_c0= 0;
+    int c1_mal_c2= 0;
+
+    int c2= 0;
+    int c2_mal_c0= 0;
+    int c2_mal_c1= 0;
+
+};
+
+int main()
+{
+    
+    vector<vector<double>> flores;
+
+    fstream file;
+
+    file.open("Iris.csv", ios::in);
+
+    if (file.is_open())
+    {
+        // voy a asumir que en ram_fichero
+
+        string linea, word;
+
+        getline(file, linea); // la primera es de las columnas no necesarias
+
+        while (getline(file, linea))
+        {
+
+            stringstream s(linea);
+
+            // cout << linea << endl << endl;
+
+            // me quedo con los datos 1, 3, 4, ....
+
+            // o sea obvio 0 y 2
+
+            string palabra;
+            int numerodato = 0;
+
+            int numeroreal = 0;
+
+            vector<double> flor;
+            for (int i = 0; i < linea.length(); i++)
+            {
+
+                if (linea[i] != ',')
+                    palabra += linea[i];
+
+                else
+                {
+
+                    // como esto no es editable y lo voy a dejar asi no voy a hacer un vector de parametros evitables (simplicidad)
+                    if (!(numerodato == 0)) // el id da igual porque solo va en orden
+                    {
+                       cout << numeroreal << " " << palabra << "|" << endl;
+
+                        double dato = arreglodato(palabra, numeroreal);
+                        flor.push_back(dato);
+
+                        numeroreal++;
+                    }
+
+                    numerodato++;
+                    palabra = "";
+                }
+            }
+            cout << palabra << endl << endl;
+            
+            // aqui es de la clasificación, ONE HOT
+            double dato = arreglodato(palabra, numeroreal);
+            vector<double> onehot;
+
+            cout << "PLANTA ES TIPO " << dato << endl;
+            if (dato == 0){
+                onehot = {
+                    1, 0, 0
+                };
+
+                double res = 0;
+
+                flor.push_back(1.0);
+                flor.push_back(0.0);
+                flor.push_back(0.0);
+            }
+            else if (dato == 1){
+                onehot = {
+                    0, 1, 0
+                };
+
+                
+                flor.push_back(0.0);
+                flor.push_back(1.0);
+                flor.push_back(0.0);
+            }
+            else if(dato == 2){
+                onehot = {
+                    0, 0, 1
+                };
+
+                flor.push_back(0.0);
+                flor.push_back(0.0);
+                flor.push_back(1.0);
+            }
+            else{
+                onehot = {
+                    0, 0, 1
+                };
+
+                flor.push_back(0.0);
+                flor.push_back(0.0);
+                flor.push_back(1.0);
+            }
+
+            //flor.push_back(onehot);
+
+            flores.push_back(flor);
+            
+            
+
+        
+            //personas.push_back(per);
+        }
+    }
+
+
+    random_device rd;
+    mt19937 gen(rd());
+    shuffle(flores.begin(), flores.end(), gen);
+
+
+
+    vector<vector<double>> flores_input;
+    vector<vector<double>> flores_output;
+
+    for(int i = 0; i < flores.size(); i++){
+
+        int numvalor = 0;
+        vector<double> input;
+        vector<double> output;
+        for(int j = 0; j < flores[i].size(); j++){
+
+            if (numvalor == 0 || numvalor == 1 || numvalor == 2 || numvalor == 3){
+                input.push_back(flores[i][j]);
+            }
+            else{
+                output.push_back(flores[i][j]);
+            }
+            numvalor++;
+
+        }
+
+        flores_input.push_back(input);
+        flores_output.push_back(output);
+    }
+
+   
+    for (size_t i = 0; i < flores_input.size(); i++)
+    {
+        cout << "FLOR " << i << ": ";
+
+        for (size_t j = 0; j < flores_input[i].size(); j++)
+        {
+            cout << flores_input[i][j] << " ";
+        }
+
+        cout << endl;
+    }
+
+
+    for (size_t i = 0; i < flores_output.size(); i++)
+    {
+        cout << "FLOR " << i << ": ";
+
+        for (size_t j = 0; j < flores_output[i].size(); j++)
+        {
+            cout << flores_output[i][j] << " ";
+        }
+
+        cout << endl;
+    }
+
+
+
+
+    vector<vector<double>> X_train, Y_train, X_test, Y_test;
+    double porcentaje_train = 70;
+    double porcentaje_test = 100.0 - porcentaje_train;
+    
+    double numero_train = flores_input.size() * (porcentaje_train / 100);
+    double numero_test = flores_input.size() * (porcentaje_test / 100);
+
+    cout << "filas entrenamiento " << numero_train << ", filas test " << numero_test << ": " << numero_train + numero_test << endl << endl;
+    
+    for (int i = 0; i < numero_train; i++){
+        
+        X_train.push_back(flores_input[i]);
+        Y_train.push_back(flores_output[i]);
+        //cout << i+1 << "," ;
+    }
+
+    cout << endl << endl << endl;
+    for (int i = flores_input.size() - numero_test; i < flores_input.size(); i++){
+        
+        X_test.push_back(flores_input[i]);
+        Y_test.push_back(flores_output[i]);
+        //cout << i+1 << "," ;
+    }
+
+
+
+    // ENTRENAMIENTO
+    RedBackPropagation red_iris({4, 6, 3});
+    red_iris.tasa_aprendizaje = 0.1;
+    red_iris.entrenar(X_train, Y_train, 5000);
+
+
+
+
+    vector<vector<double>> predic_cat;
+
+    vector<double> pred_norm;
+    vector<double> res_norm;
+    cout << endl;
+
+    MConfusion matriz;
+
+    for(int i = 0; i < numero_test; i++){
+        predic_cat.push_back(red_iris.forward(X_test[i]));
+        
+        
+            int mayor = mayor_valor(predic_cat[i][0], predic_cat[i][1], predic_cat[i][2]);
+
+            int mayor_real = mayor_valor(Y_test[i][0], Y_test[i][1], Y_test[i][2]);
+            // mayor = 0 (primero), 1 (segundo) , 2(tercero)
+
+            cout << "PRED " << i << ": " << predic_cat[i][0] << ","<< predic_cat[i][1] << "," << predic_cat[i][2] << ": " <<mayor << " Y " << Y_test[i][0] << "," << Y_test[i][1] << "," << Y_test[i][2] << endl;
+            pred_norm.push_back(mayor);
+
+
+            if(mayor_real == mayor){
+                if(mayor_real == 0){
+                    matriz.c0++;
+                }
+                else if (mayor_real == 1){
+                    matriz.c1++;
+                }
+                else if(mayor_real == 2){
+                    matriz.c2++;
+                }
+                else{
+                    matriz.c2++;
+                }
+            }
+            else{
+
+                if(mayor_real == 0 && mayor == 1){
+                    matriz.c0_mal_c1++;
+                }
+                else if (mayor_real == 0 && mayor == 2){
+                    matriz.c0_mal_c2++;
+                }
+                else if (mayor_real == 1 && mayor == 0){
+                    matriz.c1_mal_c0++;
+                }
+                else if (mayor_real == 1 && mayor == 2){
+                    matriz.c1_mal_c2++;
+                }
+                else if (mayor_real == 2 && mayor == 0){
+                    matriz.c2_mal_c0++;
+                }
+                else if (mayor_real == 2 && mayor == 1){
+                    matriz.c2_mal_c1++;
+                }
+                else{
+                    cout << "no otro caso";
+                }
+            }
+
+        
+        }
+    
+
+    cout << endl;
+    cout << "WARNING!!!: las clases cambian porque cada vez que se compila, se mezclan todas las flores, las predicciones se hacen sobre flores diferentes cada vez" << endl;
+    cout << "MATRIZ DE CONFUSION DE CLASIFICACIÓN DE IRIS" << endl;
+
+    cout << "CLASE 0    " << matriz.c0 << "          " << matriz.c0_mal_c1 << "          " << matriz.c0_mal_c2 << endl;
+    cout << "CLASE 1    " <<matriz.c1_mal_c0 << "          " << matriz.c1 << "          " << matriz.c1_mal_c2 << endl;
+    cout << "CLASE 2    " <<matriz.c2_mal_c0 << "          " << matriz.c2_mal_c1 << "          " << matriz.c2 << endl;
+
+
+
+    cout << "           CLASE 0    CLASE 1     CLASE 2     (clase ficticia)" << endl;
+
+    cout << endl << "ÍNDICE" << endl;
+    cout << "CLASE O: Iris-setosa" << endl <<  "CLASE 1: Iris-versicolor" << endl << "CLASE 2: Iris-virginica" << endl;
+ 
+    /*
+
+    Iris-setosa
+    Iris-versicolor
+    Iris-virginica
+
+*/
+    
+    return 0;
+}
