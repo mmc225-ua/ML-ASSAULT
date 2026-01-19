f346bfb443de57c2bbc709603edaa53e9d196891
mmc225-ua <mmc225@gcloud.ua.es>
2026-01-19 18:48:56 +0100

    Actualizado miguel

--- Arcade-Learning-Environment-0.6.1/Innov-8-Bit-P2/perceptron_runner.cpp
+++ Arcade-Learning-Environment-0.6.1/Innov-8-Bit-P2/perceptron_runner.cpp
@@ -524,6 +524,16 @@ struct Args {
     int seed = 0;
 };
 
+
+static void usage(const char* prog) {
+    cerr << "Uso:\n";
+    cerr << "  " << prog
+         << " --csv <ruta> --target <col> --classes <c1,c2,...>\n"
+         << "    [--drop <col1,col2,...>] [--date_cols <col1,...>] [--cat_cols <col1,...>]\n"
+         << "    [--epochs N] [--lr X] [--test_ratio R] [--seed S]\n";
+    exit(1);
+}
+
 //Lee los argumentos de consola, los valida, y devuelve una estructura Args con todo
 static Args parse_args(int argc, char** argv) {
     Args a;
